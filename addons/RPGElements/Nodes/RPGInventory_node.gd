3d266539da152a88bbc2734749423bc3bbf7ad71
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-04-01 23:22:45 -0300

    Refactorizando y empezando a crear el plugin.

---
+++ addons/RPGElements/Nodes/RPGInventory_node.gd
@@ -0,0 +1,14 @@
+tool
+extends Node
+
+var inv = []
+
+# Los tipos de inventarios son el modo en el cuál se organizan los items,
+# por el momento solo se pueden organizar por peso. (TYPE_BY_WEIGHT)
+enum Type {TYPE_BY_WEIGHT}
+
+func add_item():
+	pass
+	
+func get_item():
+	pass
--- addons/RPGElements/plugin.gd
+++ addons/RPGElements/plugin.gd
@@ -2,10 +2,13 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-#	add_custom_type("Persistence", "Node", preload("persistence_node.gd"), preload("icon.png"))
-	# add_autoload_singleton(name, path) # 3.1
-	pass
+	add_custom_type("RPGPlayer", "Node", preload("Nodes/RPGPlayer_node.gd"), preload("Icons/RPGPlayer_node.png"))
+	add_custom_type("RPGStats", "Node", preload("Nodes/RPGStats_node.gd"), preload("Icons/RPGStats_node.png"))
+	add_custom_type("RPGInventory", "Node", preload("Nodes/RPGInventory_node.gd"), preload("Icons/RPGInventory_node.png"))
+	add_custom_type("RPGItem", "Node", preload("Nodes/RPGItem_node.gd"), preload("Icons/RPGItem_node.png"))
 
 func _exit_tree():
-#	remove_custom_type("Persistence")
-	pass
+	remove_custom_type("RPGPlayer")
+	remove_custom_type("RPGStats")
+	remove_custom_type("RPGInventory")
+	remove_custom_type("RPGItem")
