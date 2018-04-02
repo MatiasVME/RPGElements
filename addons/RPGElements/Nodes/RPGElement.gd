8c88ca9789a1e073eb609872edab60271bb6ea14
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-04-02 18:36:00 -0300

    Añadiendo la curva de experiencia al player y la licencia a los distintos archivos

--- addons/RPGElements/Nodes/RPGElement.gd
+++ addons/RPGElements/Nodes/RPGElement.gd
@@ -20,39 +20,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-tool
-extends "RPGElement.gd"
+extends Node
 
-# Los tipos de inventarios son el modo en el cuál se organizan los items,
-# por el momento solo se pueden organizar por peso. (TYPE_BY_WEIGHT).
-# TODO: TYPE_BY_SLOTS : A futuro agregar la opción de almacenar por slots
-enum Type {TYPE_BY_WEIGHT}
-export (Type) var type = TYPE_BY_WEIGHT
+export (bool) var debug = true
 
-var inv = []
-
-# Métodos Públicos
-#
-
-func add_item(item):
-	match type:
-		TYPE_BY_WEIGHT:
-			add_item_by_weight(item)
-
-# Setters/Getters
-#
-
-func set_serialized(dict):
-	pass
-
-func get_serialized():
-	pass
-
-func get_item():
-	pass
-	
-# Métodos "Privados"
-#
-
-func add_item_by_weight(item):
-	pass
+func debug(message, something1 = "", something2 = ""):
+	if debug:
+		print("[RPGElements] ", message, " ", something1, " ", something2)
