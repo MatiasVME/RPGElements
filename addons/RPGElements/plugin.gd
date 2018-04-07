5359189f8e25aa3b4136e25a569e2a72e7bdff30
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-04-07 11:36:07 -0300

    Mas refactorización y se añade un nuevo nodo tipo dialogo

--- addons/RPGElements/plugin.gd
+++ addons/RPGElements/plugin.gd
@@ -2,13 +2,15 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("RPGPlayer", "Node", preload("Nodes/RPGPlayer.gd"), preload("Icons/RPGPlayer_node.png"))
-	add_custom_type("RPGStats", "Node", preload("Nodes/RPGStats.gd"), preload("Icons/RPGStats_node.png"))
-	add_custom_type("RPGInventory", "Node", preload("Nodes/RPGInventory.gd"), preload("Icons/RPGInventory_node.png"))
-	add_custom_type("RPGItem", "Node", preload("Nodes/RPGItem.gd"), preload("Icons/RPGItem_node.png"))
+	add_custom_type("RPGPlayer", "Node", preload("Nodes/RPGPlayer.gd"), preload("Icons/RPGPlayer.png"))
+	add_custom_type("RPGStats", "Node", preload("Nodes/RPGStats.gd"), preload("Icons/RPGStats.png"))
+	add_custom_type("RPGInventory", "Node", preload("Nodes/RPGInventory.gd"), preload("Icons/RPGInventory.png"))
+	add_custom_type("RPGItem", "Node", preload("Nodes/RPGItem.gd"), preload("Icons/RPGItem.png"))
+	add_custom_type("RPGDialog", "Node", preload("Nodes/RPGDialog.gd"), preload("Icons/RPGDialog.png"))
 
 func _exit_tree():
 	remove_custom_type("RPGPlayer")
 	remove_custom_type("RPGStats")
 	remove_custom_type("RPGInventory")
 	remove_custom_type("RPGItem")
+	remove_custom_type("RPGDialog")
