eaa8cc76c3856cebcad0c4b628a1408886167a19
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-05-18 18:09:02 -0400

    Trabajando en las stats

--- Examples/ExampleStats/ExampleStats.gd
+++ Examples/ExampleStats/ExampleStats.gd
@@ -1,18 +1,29 @@
 extends Node
 
 func _ready():
-	$RPGStats.get_stat_value("hola")
-	$RPGStats.add_stat("hola", 3)
-	print($RPGStats.get_stat_value("hola"))
+	print("$RPGStats.get_stat_value(hola)", $RPGStats.get_stat_value("hola"))
 	
-#	var dict = {
-#		Stat1 = 1,
-#		Stat2 = 2,
-#		Stat3 = 3
-#	}
-#
-#	$RPGStats.set_serialized(dict)
-#
-#	print($RPGStats.get_serialized())
+	$RPGStats.add_stat("hola", 50)
 	
+	print("$RPGStats.get_stat_value(hola): ", $RPGStats.get_stat_value("hola"))
+	print("$RPGStats.get_stat(hola): ", $RPGStats.get_stat("hola"))
 	
+	var dict = {
+		Stat1 = 1,
+		Stat2 = 2,
+		Stat3 = 3
+	}
+
+	$RPGStats.set_serialized(dict)
+
+	print("$RPGStats.get_serialized(): ", $RPGStats.get_serialized())
+	
+	$RPGStats.add_points(5)
+	$RPGStats.add_points_to_stat(3, "Stat1")
+	
+	print("$RPGStats.get_serialized(): ", $RPGStats.get_serialized())
+	
+	$RPGStats.remove_points_to_stat(100, "Stat1")
+	$RPGStats.remove_points_to_stat(1, "Stat2")
+	
+	print("$RPGStats.get_serialized(): ", $RPGStats.get_serialized())
--- addons/RPGElements/Nodes/RPGStats.gd
+++ addons/RPGElements/Nodes/RPGStats.gd
@@ -23,41 +23,84 @@
 tool
 extends "RPGElement.gd"
 
-var stats = []
-var points = 0
+# Almacena las stats en si y los puntos relacionados con la stat, por
+# ejemplo: [["Fuerza", 3], ["Inteligencia", 2]].
+var stats = [] # No se debe editar directamente
+# Puntos que se acumulan para añadir a las stats
+var points = 0 setget , get_points # No se debe editar directamente
 
 # Métodos Públicos
 #
 
+# Añadir una stat con una determinada cantidad de puntos
 func add_stat(stat_name, points):
 	stats.append([stat_name, points])
-	
+
+# Añadir puntos para acumular en points, estos puntos pueden ser usados
+# para ser añadidos con el método add_points_to_stat()
 func add_points(_points):
-	points += _points
-	
+	if _points > 0:
+		points += _points
+	else:
+		.debug("_points debe ser mayor que 0")
+
+# Remueve una cantidad de puntos que son acumulados en points.
+func remove_points(_points):
+	if _points > 0:
+		points -= _points
+	else:
+		.debug("_points debe ser mayor que 0")
+
+# Toma puntos de points y los añade a x stat
 func add_points_to_stat(cant, stat_name):
-	if not cant > points:
-		pass #TODO
+	if cant < points:
+		points -= cant
+		get_stat(stat_name)[1] += cant
 	else:
 		.debug("La cantidad dada, supera a la cantidad de puntos")
 
+# Quita puntos a una stat y los guarda en points
+func remove_points_to_stat(cant, stat_name):
+	var stat = get_stat(stat_name)
+	
+	if cant > 0 and cant <= stat[1]:
+		stat[1] -= cant
+		points += cant
+	else:
+		.debug("No se puede remover esa cantidad de puntos      ")
+
+# Borra todas las stats
 func clear_stats():
 	stats.clear()
 	stats = []
 
-
-
 # Setters/Getters
 #
 
+func get_points():
+	return points
+
+# Retorna el valor de la stat
 func get_stat_value(stat_name):
 	# range() no devuelve el último valor
 	for i in range(0, stats.size()):
 		if stats[i][0] == stat_name:
-			return stats[i][1] # Retorna el valor de la stat
+			return stats[i][1] 
 
 	.debug("No se ecuentra stat_name: ", stat_name)
-	
+
+# Retorna el arreglo de la stat
+func get_stat(stat_name):
+	# range() no devuelve el último valor
+	for i in range(0, stats.size()):
+		if stats[i][0] == stat_name:
+			return stats[i] 
+
+	.debug("No se ecuentra stat_name: ", stat_name)
+
+func get_stats():
+	return stats
+
 # Para ser serializado debe de ser un diccionario con las siguientes
 # características.
 # dict = {
