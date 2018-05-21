f9c2754f5a1c21ae9184f712d891dc4ab4f88a78
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-05-20 20:34:10 -0400

    Trabajando en el RPGCharacter

--- addons/RPGElements/Nodes/RPGCharacter.gd
+++ addons/RPGElements/Nodes/RPGCharacter.gd
@@ -30,9 +30,11 @@ var level_max = 30
 
 # Vitalidad
 var hp = 0 setget set_hp, get_hp
+var max_hp = 10 setget set_max_hp, get_max_hp
 # Mana
-var energy = 0
-var defense = 0
+var energy = 0 setget set_energy, get_energy
+var max_energy = 10 setget set_max_energy, get_max_energy
+# var defense = 0 # TODO: Implementar defensa en un futuro
 
 var xp = 0
 # En xp_required 0 equivale a level 1
@@ -40,22 +42,33 @@ var xp_required = [] setget , get_xp_required
 
 signal level_up
 signal get_xp
-# signal get_damage
-# signal consume_energy
-# signal consume_defense
+signal add_hp
+signal remove_hp
+signal full_hp
+signal dead
+signal add_energy
+signal remove_energy
+signal full_energy
+signal no_energy
+# signal add_defense # TODO
+# signal remove_defense # TODO
 
 func _ready():
 	create_default_xp_curve()
 	
 	# Signals
-	connect("level_up", self, "_on_level_up")
-	connect("get_xp", self, "_on_get_xp")
-
-func _on_level_up():
-	debug("Level Up!!: ", level)
-	
-func _on_get_xp():
-	debug("Get XP")
+	if debug:
+		connect("level_up", self, "_on_level_up")
+		connect("get_xp", self, "_on_get_xp")
+		connect("add_hp", self, "_on_add_hp")
+		connect("remove_hp", self, "_on_remove_hp")
+		connect("full_hp", self, "_on_full_hp")
+		connect("dead", self, "_on_dead")
+		connect("add_energy", self, "_on_add_energy")
+		connect("remove_energy", self, "_on_remove_energy")
+		connect("full_energy", self, "_on_full_energy")
+		connect("no_energy", self, "_on_no_energy")
+#		connect("", self, "_on_")
 
 # Métodos Públicos
 #
@@ -88,7 +101,7 @@ func add_xp(_xp):
 			emit_signal("level_up")
 		else:
 			break
-			
+		
 	xp += _xp
 	debug(xp)
 	
@@ -97,6 +110,44 @@ func remove_xp(_xp):
 		xp -= _xp
 	else:
 		xp = 0
+	
+func add_hp(_hp):
+	if hp + _hp < max_hp:
+		hp += _hp
+		emit_signal("add_hp")
+	else: # Significa que se esta añadiendo más hp de lo que se podría
+		hp = max_hp
+		emit_signal("full_hp")
+	
+func remove_hp(_hp):
+	if _hp > 0:
+		if hp - _hp > 0:
+			hp -= _hp
+			emit_signal("remove_hp")
+		else:
+			hp = 0
+			emit_signal("dead")
+	else:
+		.debug("No se puede eliminar esa cantidad de HP")
+	
+func add_energy(_energy):
+	if energy + _energy < max_energy:
+		energy += _energy
+		emit_signal("add_energy")
+	else:
+		energy = max_energy
+		emit_signal("full_energy")
+
+func remove_energy(_energy):
+	if _energy > 0:
+		if energy - _energy > 0:
+			energy -= _energy
+			emit_signal("remove_energy")
+		else:
+			energy = 0
+			emit_signal("no_energy")
+	else:
+		.debug("No se puede eliminar esa cantidad de energía")
 
 # Setters/Getters
 #
@@ -119,8 +170,70 @@ func get_player_name():
 func set_hp(_hp):
 	hp = _hp
 	
+	if hp > max_hp:
+		hp = max_hp
+		emit_signal("full_hp")
+	elif hp <= 0:
+		hp = 0
+		emit_signal("dead")
+	
 func get_hp():
 	return hp
+	
+func set_max_hp(_max_hp):
+	if _max_hp > 0:
+		max_hp = _max_hp
+	else:
+		.debug("_max_hp tiene que ser mayor que 0")
+	
+func get_max_hp():
+	return max_hp
+
+func set_energy(_energy):
+	energy = _energy
+	
+	if energy > max_energy:
+		energy = max_energy
+		emit_signal("full_energy")
+	elif energy <= 0:
+		energy = 0
+		emit_signal("no_energy")
+	
+func get_energy():
+	return energy
+
+# Señales
+#
+
+func _on_level_up():
+	debug("Level Up!!: ", level)
+	
+func _on_get_xp():
+	debug("Get XP: ", xp)
+	
+func _on_add_hp():
+	pass
+	
+func _on_remove_hp():
+	pass
+	
+func _on_full_hp():
+	pass
+	
+func _on_dead():
+	pass
+
+func _on_add_energy():
+	pass
+
+func _on_remove_energy():
+	pass
+	
+func _on_full_energy():
+	pass
+	
+func _on_no_energy():
+	pass
 
 # Métodos "Privados"
 #
